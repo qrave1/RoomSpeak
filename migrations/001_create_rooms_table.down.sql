cfdf01f2568f056f0028bc83ef6720ec89958634
asmirnov <asmirnov@team.amocrm.com>
2025-08-26 19:08:43 +0300

    rm

--- migrations/001_create_rooms_table.down.sql
+++
@@ -1,2 +0,0 @@
-DROP INDEX IF EXISTS idx_rooms_room_id;
-DROP TABLE IF EXISTS rooms; 
--- migrations/001_create_rooms_table.up.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS rooms (
-    id SERIAL PRIMARY KEY,
-    room_id VARCHAR(255) UNIQUE NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP DEFAULT NOW()
-);
-
-CREATE INDEX idx_rooms_room_id ON rooms(room_id); 
